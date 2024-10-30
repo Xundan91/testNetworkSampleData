4c072e51eb75e22ac4e651c31c39031f41bc1307
kundan kumar <kundansingh023230@gmail.com>
2024-10-30 15:37:29 +0530

    sample data

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const app = express()
+let data = {
+    network : 11,
+    jobs : 12,
+    message : 11 ,
+    notifications : 23
+}
+
+app.get('/',(req,res)=>{
+    res.json(data)
+
+})
+
+app.get('/update',(req,res)=>{
+    data = {
+        network : 100,
+        jobs : 11,
+        message : 89,
+        notifications : 1000
+
+    }
+    res.json(data)    
+})
+
+app.listen(3000,()=>{
+    console.log("server Running");
+    
+})
