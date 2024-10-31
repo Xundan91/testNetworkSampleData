4007b5decc61e1a90891872967a1ae0c0204408d
kundan kumar <kundansingh023230@gmail.com>
2024-10-31 21:28:32 +0530

    done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const cors = require('cors');
-app.use(cors({ origin: 'http://127.0.0.1:5173' }));
+app.use(cors());
 
 let data = {
     network : 11,
